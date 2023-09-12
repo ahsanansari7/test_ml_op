fd889710c389d30483fe090f0bdd54dccae0fadd
Ahsan <ansariahsan512@gmail.com>
2023-09-12 12:32:44 +0500

    project initiated successfully

---
+++ README.md
@@ -0,0 +1,1 @@
+# A complete Life cycle of Data Science
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def get_requirements(file_path):
+    requirements = []
+
+    with open (file_path,'r') as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+    
+    return requirements
+
+
+setup(
+
+name = "datascienceproject",
+version = "0.0.1",
+author =  "ahsan ansari",
+author_email = "ansariahsan512@gmail.com",
+packages = find_packages(),
+install_requirements = get_requirements("requirements.txt")
+
+)
